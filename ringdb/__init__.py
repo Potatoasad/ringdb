9f17e68df09ff98a97bc40f55302f7140d1a44c4
Asad Hussain <asadhussain625@gmail.com>
2022-02-24 19:30:21 -0600

    Gave more reasonable names to the url data frames

--- ringdb/__init__.py
+++ ringdb/__init__.py
@@ -26,17 +26,13 @@ except ImportError:
     # Try backported to PY<37 `importlib_resources`.
     import importlib_resources as pkg_resources
 
-from . import metadb  # relative-import the *package* containing the templates
+from . import metadb 
 
 posterior_url_path = pkg_resources.open_text(metadb, 'posterior_urls.csv')
-# or for a file-like stream:
 strain_url_path = pkg_resources.open_text(metadb, 'strain_urls.csv')
 
-#posterior_url_path = os.path.join(os.getcwd(), 'ringdb/metadb/posterior_urls.csv')
-#strain_url_path = os.path.join(os.getcwd(), 'ringdb/metadb/strain_urls.csv')
-
-posterior_url_df = pd.read_csv(posterior_url_path)
-strain_url_df = pd.read_csv(strain_url_path)
+posterior_urls = pd.read_csv(posterior_url_path)
+strain_urls = pd.read_csv(strain_url_path)
 
 PD = PosteriorDatabase(folder_post, posterior_url_df)
 SD = StrainDatabase(folder_post, strain_url_df)
