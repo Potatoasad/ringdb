86685210b1a07c8c4e8456b274fe30dc2e242d45
potatoasad <asadhussain625@gmail.com>
2022-02-26 13:11:06 -0600

    Added the ability for Event objects to query custom h5 paths from their files

--- ringdb/Database.py
+++ ringdb/Database.py
@@ -90,4 +90,56 @@ class Event:
     def strain(self, detectors=None, duration=32.0):
         return self.SD_ref.strain(self.name, detectors=detectors, duration=duration)
 
+    def read_posterior_file(self, h5path, datatype='array', attr_name=None, detectors=None, approximant=None, replacement_dict=None):
+        # Run over the list of detectors if not provided
+        if detectors is None:
+            detectors = self.PD_ref.available_detectors(self.name)
+
+        # Choose the best approximant according to the provided list order
+        if approximant is None:
+            approximant = self.PD_ref.choose_approximant(self.name)
+
+        # Method to interpolate the path
+        if replacement_dict is None:
+            replacement_dict = {'detector': detectors,  'event': self.name, 'approximant': approximant}
+
+        # if the detectors are a list, return a list of dictionaries
+        if isinstance(detectors, list):
+            result = {}
+            for ifo in detectors:
+                scheme = {'type': datatype, 'name': attr_name, 'path': h5path}
+                file = self.PD_ref.event_path(self.name)
+                new_replacement_dict = replacement_dict.copy()
+                new_replacement_dict['detector'] = ifo
+                result[ifo] = self.PD_ref.read_data_from_file(file, scheme, new_replacement_dict)
+        else:
+            scheme = {'type': datatype, 'name': attr_name, 'path': h5path}
+            file = self.PD_ref.event_path(self.name)
+            result = self.PD_ref.read_data_from_file(file, scheme, replacement_dict)
+        return result
+
+    def read_strain_file(self, h5path, datatype='array', attr_name=None, detectors=None, replacement_dict=None):
+        # Run over the list of detectors if not provided
+        if detectors is None:
+            detectors = self.SD_ref.available_detectors(self.name)
+        
+        # Method to interpolate the path
+        if replacement_dict is None:
+            replacement_dict = {'detector': detectors,  'event': self.name}
+
+        # if the detectors are a list, return a dictionary of results labelled by detector
+        if isinstance(detectors, list):
+            result = {}
+            for ifo in detectors:
+                scheme = {'type': datatype, 'name': attr_name, 'path': h5path}
+                file = filepath = f"{self.folder}/{event}.hdf5"
+                new_replacement_dict = replacement_dict.copy()
+                new_replacement_dict['detector'] = ifo
+                result[ifo] = self.SD_ref.read_data_from_file(file, scheme, new_replacement_dict)
+        else:
+            scheme = {'type': datatype, 'name': attr_name, 'path': h5path}
+            file = filepath = f"{self.folder}/{event}.hdf5"
+            result = self.SD_ref.read_data_from_file(file, scheme, replacement_dict)
+        return result
+
         
