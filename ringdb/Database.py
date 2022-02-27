2725816cfa6dba4ba3289c00eec9e0bde231fda9
potatoasad <asadhussain625@gmail.com>
2022-02-26 18:42:13 -0600

    Added docstrings for methods in the Event class

--- ringdb/Database.py
+++ ringdb/Database.py
@@ -73,6 +73,9 @@ class Database:
     def event(self, eventname):
         return Event(eventname, self)
 
+    def event_list(self):
+        return list(self.strain_urls.Event.unique())
+
 
 class Event:
     def __init__(self, eventname, DB_reference):
@@ -82,15 +85,109 @@ class Event:
         self.SD_ref = DB_reference.StrainDB
 
     def posteriors(self):
+        """
+        Returns a dataframe of the posterior samples of the event
+
+        Returns:
+            pd.DataFrame: Posterior samples of the event with each row
+            being a posterior sample and the columns the parameter
+        """
         return self.PD_ref.posteriors(self.name)
 
     def psd(self, detector=None):
+        """
+        Returns the PSD (Power Spectral Density) estimate for all detectors
+        or a single detector if specified.
+        
+        Will download the needed PSD files if they are not already there
+
+        Args:
+            detector (None, string, or list of strings):
+                If not provided, it will return the psds of all detectors 
+                available.
+                A list of detectors whose PSDs you want can be input as
+                e.g. detector = ['H1','L1']
+                A string denoting the detector whose psd you want 'H1',
+                'L1' or 'V1'
+
+        Returns:
+            A dictionary containing ringdown.PowerSpectrum objects for
+            each detector's PSD.
+
+            If detector is not specified:
+                A dictionary labelled by detector name, containing PSDs
+                for all detectors as ringdown.PowerSpectrum objects
+
+            If detector is specified as a string (e.g. detector='H1'):
+                A ringdown.PowerSpectrum object containing the detector PSD
+        """
         return self.PD_ref.psd(self.name, detector=detector)
 
     def strain(self, detectors=None, duration=32.0):
+        """
+        Returns the strain for all detectors or a single detector 
+        if specified.
+        
+        Will download the needed hdf5 files if they are not already there
+
+        Args:
+            detector (None, string, or list of strings):
+                If not provided, it will return the strain object of all detectors 
+                available.
+                A list of detectors whose PSDs you want can be input as
+                e.g. detector = ['H1','L1']
+                A string denoting the detector whose psd you want 'H1',
+                'L1' or 'V1'
+
+            duration (float):
+                default is 32.0s. You can ask for a 4096.0s strain as well
+                but as of now it probably will not overwrite what you have.
+                This is definitely on the next TODO.
+
+        Returns:
+            A dictionary containing ringdown.PowerSpectrum objects for
+            each detector's PSD.
+
+            If detector is not specified:
+                A dictionary labelled by detector name, containing PSDs
+                for all detectors as ringdown.PowerSpectrum objects
+
+            If detector is specified as a string (e.g. detector='H1'):
+                A ringdown.PowerSpectrum object containing the detector PSD
+        """
         return self.SD_ref.strain(self.name, detectors=detectors, duration=duration)
 
     def read_posterior_file(self, h5path, datatype='array', attr_name=None, detectors=None, approximant=None, replacement_dict=None):
+        """
+        Returns the referenced data object in the posterior hdf5 file for all detectors or a single detector 
+        if specified.
+
+        Will not work for GWTC-1 events, and will only work once the file 
+        is already downloaded
+
+        Args:
+            h5path (string):
+                Internal path for data you want from a particular hdf5 file.
+                Use '{detector}', '{approximant}', '{event}' as wildcards to 
+                be filled in with the appropriate values for this event. 
+                If {detector} is used as a wildcard, results for each appropriate
+                detector will be returned.
+
+            datatype (string):
+                Can be one of ('array', 'value', 'attribute').
+                If it is an attribute, you must also provide attr_name='name-of-attribute'
+
+        Returns:
+            A dictionary containing the asked for objects for
+            each detector's PSD.
+
+            If detector is not specified:
+                A dictionary labelled by detector name, containing objects 
+                for each detector
+
+            If detector is specified as a string (e.g. detector='H1'):
+                returns the object for that one detector value
+        """
         # Run over the list of detectors if not provided
         if detectors is None:
             detectors = self.PD_ref.available_detectors(self.name)
@@ -119,6 +216,36 @@ class Event:
         return result
 
     def read_strain_file(self, h5path, datatype='array', attr_name=None, detectors=None, replacement_dict=None):
+        """
+        Returns the referenced data object in the strain hdf5 file for all detectors or a single detector 
+        if specified.
+
+        Will not work for GWTC-1 events, and will only work once the file 
+        is already downloaded
+
+        Args:
+            h5path (string):
+                Internal path for data you want from a particular hdf5 file.
+                Use '{detector}', '{event}' as wildcards to 
+                be filled in with the appropriate values for this event. 
+                If {detector} is used as a wildcard, results for each appropriate
+                detector will be returned.
+
+            datatype (string):
+                Can be one of ('array', 'value', 'attribute').
+                If it is an attribute, you must also provide attr_name='name-of-attribute'
+
+        Returns:
+            A dictionary containing the asked for objects for
+            each detector's PSD.
+
+            If detector is not specified:
+                A dictionary labelled by detector name, containing objects 
+                for each detector
+
+            If detector is specified as a string (e.g. detector='H1'):
+                returns the object for that one detector value
+        """
         # Run over the list of detectors if not provided
         if detectors is None:
             detectors = self.SD_ref.available_detectors(self.name)
