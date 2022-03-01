821017024b89823f53ee45aeb10520353568a0c9
potatoasad <asadhussain625@gmail.com>
2022-03-01 00:14:16 -0600

    Cleaned up the setup.py file

--- setup.py
+++ setup.py
@@ -9,27 +9,20 @@ description = '''A gravitational event database that queries and locally saves e
 detector PSDs and posterior samples - all in one place'''
 
 setup(name='ringdb',
-      version='0.1.1',
+      version='0.1.2',
       description=description.replace('\n',''),
       author='Asad Hussain',
       author_email='asadh@utexas.edu',
       url='https://github.com/potatoasad/ringdb',
-      download_url='https://github.com/Potatoasad/ringdb/archive/refs/tags/v0.1.1.zip',
+      download_url='https://github.com/Potatoasad/ringdb/archive/refs/tags/v0.1.2.zip',
       license='MIT',
       packages=['ringdb'],
       package_data={'ringdb': ['metadb/*']},
       install_requires=[
-            'Cython>=0.22',
-            'arviz',
             'h5py',
             'lalsuite',
             'matplotlib',
             'numpy',
             'pandas',
-            'pystan>=2,<3',
-            'qnm',
-            'scipy',
-            'seaborn',
-            'gwosc',
             'ringdown']
      )
