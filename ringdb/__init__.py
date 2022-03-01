ce5f68cfde92ca56d0c6404861e7942fb73571cb
potatoasad <asadhussain625@gmail.com>
2022-03-01 00:26:58 -0600

    cleaned up init file to speed up first import

--- ringdb/__init__.py
+++ ringdb/__init__.py
@@ -24,8 +24,6 @@ from . import StrainDatabase
 from . import PosteriorDatabase
 from . import Database
 
-folder_post = "./TestingNew/Data/PosteriorData"
-
 from . import metadb
 
 try:
@@ -34,15 +32,3 @@ except ImportError:
     # Try backported to PY<37 `importlib_resources`.
     import importlib_resources as pkg_resources
 
-from . import metadb 
-
-posterior_url_path = pkg_resources.open_text(metadb, 'posterior_urls.csv')
-strain_url_path = pkg_resources.open_text(metadb, 'strain_urls.csv')
-psd_url_path = pkg_resources.open_text(metadb, 'psd_urls.csv')
-
-posterior_urls = pd.read_csv(posterior_url_path)
-strain_urls = pd.read_csv(strain_url_path)
-psd_urls = pd.read_csv(psd_url_path)
-
-PD = PosteriorDatabase(folder_post, posterior_urls, psd_urls, strain_urls)
-SD = StrainDatabase(folder_post, strain_urls)
