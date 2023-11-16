c673a267da8767323e8d805a2c9e0b3fff959801
Asad Hussain <asadhussain625@gmail.com>
2023-11-16 01:06:44 -0600

    Update setup.py: no description

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ from setuptools import setup, find_packages
 #with open("README.md", "r", encoding="utf-8") as fh:
 #    long_description = "test"
 
-description = '''A gravitational event database that queries and locally saves event strain samples, 
-detector PSDs and posterior samples - all in one place'''
+#description = '''A gravitational event database that queries and locally saves event strain samples, 
+#detector PSDs and posterior samples - all in one place'''
 
 setup(name='ringdb',
-      version='0.1.3.3',
-      description=description.replace('\n',''),
+      version='0.1.3.4',
+      #description=description.replace('\n',''),
       author='Asad Hussain',
       author_email='asadh@utexas.edu',
       url='https://github.com/potatoasad/ringdb',
-      download_url='https://github.com/Potatoasad/ringdb/archive/refs/tags/v0.1.3.3.zip',
+      download_url='https://github.com/Potatoasad/ringdb/archive/refs/tags/v0.1.3.4.zip',
       license='MIT',
       packages=['ringdb'],
       package_data={'ringdb': ['metadb/*']},
